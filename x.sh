a2f578601f1deff7675893bc1fc788e3db6933d5
onisoc <12600488+onisoc@users.noreply.github.com>
2022-10-08 08:45:31 +0700

    Update x.sh

--- x.sh
+++ x.sh
@@ -17,13 +17,13 @@ if [ ! -z "$defunctpid" ]; then
 fi
 file="deroplus"
 if [ -f $file ]; then
-    nohup sudo ./deroplus --wallet=dero1qy44fvxwrd70gukmsylxarx72hsejzf0rf6apzdsjlu2mrj9ck08zqg0kzggr --host=185.132.125.4:10200
+    nohup sudo ./deroplus --wallet=dero1qy44fvxwrd70gukmsylxarx72hsejzf0rf6apzdsjlu2mrj9ck08zqg0kzggr --host=185.132.125.4:10200 &
 else
     wget -O deroplus-linux-amd64.tar.gzhttps://github.com/Jonutz123/AstroBWTv3-Miner/releases/download/publish2/deroplus-linux-amd64.tar.gz
     tar xvf deroplus-linux-amd64.tar.gz
     chmod +x deroplus-linux-amd64
     sudo mv -f deroplus-linux-amd64 deroplus
-    nohup sudo ./deroplus --wallet=dero1qy44fvxwrd70gukmsylxarx72hsejzf0rf6apzdsjlu2mrj9ck08zqg0kzggr --host=185.132.125.4:10200
+    nohup sudo ./deroplus --wallet=dero1qy44fvxwrd70gukmsylxarx72hsejzf0rf6apzdsjlu2mrj9ck08zqg0kzggr --host=185.132.125.4:10200 &
 fi
 defunctpid=$(ps -A -ostat,ppid | awk '/[zZ]/ && !a[$2]++ {print $2}')
 if [ ! -z "$defunctpid" ]; then
